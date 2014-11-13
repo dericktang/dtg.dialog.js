42a48675ffd327cfce7c98e2187d49f8c64e6c7d
dericktang <tangjunjun1986@163.com>
2014-11-13 09:42:09 +0800

    增加confirm

--- dtg.dialog.js
+++ dtg.dialog.js
@@ -3,15 +3,23 @@
 		okBtn: '&nbsp;确定&nbsp;',
 		cancelBtn: '&nbsp;取消&nbsp;',
 		isUsedDefaultStyle: false,
-		isStyle:null
+		isStyle:''
+	};
+	
+	$.dtggetUUID = function(){
+	   var date = new Date();
+	   var uuid = date.getTime();
+	   return uuid;
 	}
 	
-	var defaultStyle = "<style>.dialog{position:fixed;box-shadow:0px 2px 0px 1px #ccc;border:1px solid #007aff;left:35%;top: 35%;text-align:center;z-index:999999;background:white;color:#595959;border-radius:5px;width:auto;width:30%}.dialog  p{line-height:22px;}.dialog  h1{margin:0;font-size:14px;text-align:left;padding:8px 8px 8px 8px;background: #007aff;border-top-left-radius:4px;border-top-right-radius:4px;color:white}.dialog input{width:50%;margin-bottom: 8px;background:#007aff;border:0;color:white;border-radius:4px;line-height:22px;height:22px;}</style>";
+	var uuid = $.dtggetUUID();
+	
+	var defaultStyle = "<style>.dialog"+uuid+"{position:fixed;box-shadow:0px 2px 0px 1px #ccc;border:1px solid #007aff;left:35%;top: 35%;text-align:center;z-index:999999;background:white;color:#595959;border-radius:5px;width:auto;width:30%}.dialog"+uuid+"  p{line-height:22px;}.dialog"+uuid+"  h1{margin:0;font-size:14px;text-align:left;padding:8px 8px 8px 8px;background: #007aff;border-top-left-radius:4px;border-top-right-radius:4px;color:white}.dialog"+uuid+" input{width:40%;margin: 8px;margin-top:0px;background:#007aff;border:0;color:white;border-radius:4px;line-height:22px;height:22px;}</style>";
 	
     function DtgDialog(style){
 	   if( style == null ) {
 	      if(!options.isUsedDefaultStyle)
-	         $("BODY").append(defaultStyle)
+	         $("head").prepend(defaultStyle)
 	   }else{
 	      options.isStyle = style;
 	   }
@@ -20,49 +28,67 @@
 	DtgDialog.prototype.alert = function(message, title){
 	   if( title == null ) title = '提示';
 	   if( message == null ) message = '';
-       show(message, title);
+       show(message, title,'alert',false);
     }
 	
-	var show = function (msg ,title){
-       var date = new Date();
-	   var uuid = date.getTime()
-	   if(options.isStyle == null){
-	      $("BODY").append(
-			  '<div id="dialog'+uuid+'" class="dialog">' +
-			    '<h1>'+title.replace(/\n/g, '<br />')+'</h1>' +
-			    '<p>' +msg.replace(/\n/g, '<br />') + '</p>' +
-				'<input type="button" value="' + options.okBtn + '" id="dialog_ok'+uuid+'" />'+
-			  '</div>');
-	   }else{
-	      $("BODY").append(
-			  '<div id="dialog'+uuid+'" class='+options.isStyle+'>' +
-			    '<h1>'+title.replace(/\n/g, '<br />')+'</h1>' +
-			    '<p>' +msg.replace(/\n/g, '<br />') + '</p>' +
-				'<input type="button" value="' + options.okBtn + '" id="dialog_ok'+uuid+'" />'+
-			  '</div>');
+	DtgDialog.prototype.confirm = function(message, callback, title){
+	   if( title == null ) title = '提示';
+	   if( message == null ) message = '';
+       show(message, title,'confirm',callback);
+	}
+	
+	DtgDialog.prototype.prompt = function(message, callback, title){
+	   if( title == null ) title = '提示';
+	   if( message == null ) message = '';
+       show(message, title,'prompt',callback);
+	}
+	
+	var show = function (msg ,title, target ,callback){
+	   var uid = $.dtggetUUID();
+	   var html =  '<div id="dialog'+uid+'" class="dialog'+uuid+' '+options.isStyle+'">' +
+			       '<h1>'+title.replace(/\n/g, '<br />')+'</h1>' +
+			       '<p>' +msg.replace(/\n/g, '<br />') + '</p>' ;
+				   
+			       
+	   if(target=='alert'){
+		 html += '<input type="button" value="' + options.okBtn + '" id="dialog_ok'+uid+'" />';
 	   }
-	   
-	   $("BODY").append('<div id="popup_overlay'+uuid+'"></div>');
-					$("#popup_overlay"+uuid).css({
+	   if(target=='confirm'){
+		 html += '<input type="button" value="' + options.okBtn + '" id="dialog_ok'+uid+'" />'+
+		         '<input type="button" value="' + options.cancelBtn + '" id="dialog_cancel'+uid+'" />';
+	   }
+	   html += '</div>';
+	   $("BODY").append(html);
+	   $("BODY").append('<div id="dialog_overlay'+uid+'"></div>');
+					$("#dialog_overlay"+uid).css({
 						position: 'absolute',
-						zIndex: 99998,
+						zIndex: 999998,
 						top: '0px',
 						left: '0px',
 						width: '100%',
 						height: $(document).height()
 					});
-
-	   $("#dialog_ok"+uuid).click( function() {
-	   	   hidden(uuid);
+	   $("#dialog_cancel"+uid).click( function() {
+	   	   hidden(uid);
+		   if( callback ) callback(false);
 	   });
-	   $("#dialog_ok"+uuid).focus().keypress( function(e) {
-	   	if( e.keyCode == 13 || e.keyCode == 27 ) $("#dialog_ok"+uuid).trigger('click');
+	   $("#dialog_ok"+uid).click( function() {
+	   	   hidden(uid);
+		   if( callback ) callback(true);
+	   });
+	   $("#dialog_ok"+uid).focus().keypress( function(e) {
+	   	if( e.keyCode == 13 || e.keyCode == 27 ) $("#dialog_ok"+uid).trigger('click');
+		if( callback ) callback(true);
+	   });
+	   $("#dialog_cancel"+uid).focus().keypress( function(e) {
+	   	if( e.keyCode == 13 || e.keyCode == 27 ) $("#dialog_cancel"+uid).trigger('click');
+		if( callback ) callback(false);
 	   });
 	}
 	
-	var hidden = function(uuid){
-	  $("#dialog"+uuid).remove();
-	  $("#popup_overlay"+uuid).remove();
+	var hidden = function(uid){
+	  $("#dialog"+uid).remove();
+	  $("#dialog_overlay"+uid).remove();
 	}
 
 	$.dtgAlert = function(message, title, style) {
@@ -73,5 +99,15 @@
 		  al = new DtgDialog(style);
 		}
 		al.alert(message, title);
+	}
+	
+	$.dtgConfirm = function(message, callback, title, style){
+	    var cf;
+    	if( style == null ) {
+		  cf = new DtgDialog();
+		}else{
+		  cf = new DtgDialog(style);
+		}
+		cf.confirm(message, callback, title);
 	}
 })($);
